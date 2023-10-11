e6e8cc4f5c32a96baafcc938b4fceefd2acc222c
John Coene <jcoenep@gmail.com>
2023-10-12 01:21:44 +0200

    fix: change default file output name

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var dir = flag.String("dir", "R", "Directory containing files to process")
-	var out = flag.String("out", "styles.css", "Path to output CSS")
+	var out = flag.String("out", "style.min.css", "Path to output CSS file")
 	var warn = flag.Bool("warn", false, "Print warnings")
 	var verbose = flag.Bool("verbose", false, "Verbose output")
 	flag.Parse()
